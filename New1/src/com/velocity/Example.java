7027f2bf5a929626280a7d7a926f93c4ac14e4a5
Yogesh <18@Yogesh>
2022-12-21 18:01:50 +0530

    Program of HashMap

---
+++ New1/src/com/velocity/Example.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+import java.util.HashMap;
+
+public class Example {
+
+	public static void main(String[] args) {
+	HashMap h=new HashMap<>();
+	h.put(1, "Yogesh");
+	System.out.println(h);
+	}
+
+}
